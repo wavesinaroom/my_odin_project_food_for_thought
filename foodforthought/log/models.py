e2bd75b7a7adc2de5d509e3334439b3eb55adbde
wavesinaroom <jaureguij@javeriana.edu.co>
2024-03-13 15:55:22 -0500

    [Feature] Log model

--- foodforthought/log/models.py
+++ foodforthought/log/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Log(models.Model):
+   recipe_ingredients = models.JSONField();
+   recipe_area =  models.JSONField(); 
+   recipe_categories = models.JSONField();
+   activities = models.JSONField();
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs)
+        self.__class__.objects.exclude(id=self.id).delete()
+        super(Log, self).save(*args, **kwargs)
+
+    @classmethod
+    def load(cls):
+        try:
+            return cls.objects.get()
+        except cls.DoesNotExist:
+            return cls()
+
