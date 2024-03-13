ef8303e601f65213950e68e0f975873fb8af61a3
wavesinaroom <jaureguij@javeriana.edu.co>
2024-03-13 16:40:00 -0500

    [Task] Add provisional log messages

--- foodforthought/log/views.py
+++ foodforthought/log/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Log
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+def fetch_trivia_question():
+    logger.warning("NOT IMPLEMENTED: fetch_trivia_question")
+
+def check_answer():
+    logger.warning("NOT IMPLEMENTED: check_answer")
+
+def check_reward_choice():
+    logger.warning("NOT IMPLEMENTED: check_reward_choice")
+
+def award_activity():
+    logger.warning("NOT IMPLEMENTED: award_activity")
+
