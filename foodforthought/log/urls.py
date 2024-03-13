b1907331a116604d65f9e5d093e2ec9fc96379d1
wavesinaroom <jaureguij@javeriana.edu.co>
2024-03-13 17:04:56 -0500

    [Feature] Paths

--- foodforthought/log/urls.py
+++ foodforthought/log/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+        path('trivia/', views.fetch_trivia_question, name='fetch_trivia_question'),
+        path('answer/', views.check_answer, name='check_answer'),
+        path('reward/', views.check_reward_choice, name='check_reward_choice'),
+        path('activity/', views.award_activity, name='views.award_activity'),
         ]
